9e7e8ad083ee797ee5876f21cacf6197f8a3972c
williamagaly <williamsantos25@gmail.com>
2024-02-27 20:46:53 -0300

    Criando método login e CORS

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +11,45 @@ builder.Services.AddDbContext<ExoContext>(options =>
 
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
+
+// Forma de autenticação.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "JwtBearer";
+    options.DefaultChallengeScheme = "JwtBearer";
+})
+// Parâmetros de validação do token.
+.AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // Valida quem está solicitando.
+        ValidateIssuer = true,
+        // Valida quem está recebendo.
+        ValidateAudience = true,
+        // Define se o tempo de expiração será validado.
+        ValidateLifetime = true,
+        // Criptografia e validação da chave de autenticação.
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao")),
+        // Valida o tempo de expiração do token.
+        ClockSkew = TimeSpan.FromMinutes(30),
+        // Nome do issuer, da origem.
+        ValidIssuer = "exoapi.webapi",
+        // Nome do audience, para o destino.
+        ValidAudience = "exoapi.webapi"
+    };
+});
+
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
 builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
-
 
 var app = builder.Build();
 
 app.UseRouting();
+// Habilita a autenticação.
+app.UseAuthentication();
+// Habilita a autorização.
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
